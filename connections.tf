bdbf06f8061f3569898f4aaab9bdda0429f112cb
larcas01 <casey.largent@csgi.com>
2019-10-10 16:30:35 -0500

    Finished AWS Networking

--- connections.tf
+++ connections.tf
@@ -1,17 +1,4 @@
-provider "google" {
-    credentials = "${file("../account.json")}"
-    project = "thrashingcorecode"
-    region = "us-west1"
-}
-
 provider "aws" {
-    region = "us-west-1"
+    region = "us-east-1"
     profile = "default"
-}
-
-provider "azurerm" {
-    subscription_id = "0"
-    client_id = "1"
-    client_secret = "2"
-    tenant_id = "3"
 }
